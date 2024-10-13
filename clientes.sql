1a2f7d536c1b1d37c9841262d22d927186403916
jhonsitomr <jhonsdarling1717@gmail.com>
2024-10-13 13:03:39 -0400

    Update clientes.sql

--- clientes.sql
+++ clientes.sql
@@ -1,6 +1,5 @@
-CREATE TABLE Agentes (
-  ID_Agente SERIAL PRIMARY KEY,
+CREATE TABLE Clientes (
+  ID_Cliente SERIAL PRIMARY KEY,
   Nombre VARCHAR(100),
   Email VARCHAR(100),
-  Teléfono VARCHAR(15)
-)
+  Teléfono VARCHAR(15))
